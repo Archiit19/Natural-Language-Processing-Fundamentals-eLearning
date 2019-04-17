ff8b3927c0caa543476ed2da0bdb5dc8d718e547
Archit Agarwal <engg.archit@gmail.com>
2019-04-17 23:28:10 +0530

    Bug Fixes - Exercise 13,14,27,28,30,34,39,40,41,48,51,53,57,59,60,61,62 and Activity -1,5,7,9,12 and SpacyExample python file and moving data folder from Lesson 8 inside completed_notebooks

--- Lesson8/spacy_examples.py
+++ Lesson8/spacy_examples.py
@@ -2,6 +2,7 @@
 This example shows how to use an LSTM sentiment classification model trained using Keras in spaCy. spaCy splits the document into sentences, and each sentence is classified using the LSTM. The scores for the sentences are then aggregated to give the document score. This kind of hierarchical model is quite difficult in "pure" Keras or Tensorflow, but it's very effective. The Keras example on this dataset performs quite poorly, because it cuts off the documents so that they're a fixed size. This hurts review accuracy a lot, because people often summarise their rating in the final sentence
 
 Prerequisites:
+pip install cytoolz
 spacy download en_vectors_web_lg
 pip install keras==2.0.9
 
